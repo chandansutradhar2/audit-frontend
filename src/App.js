28a6f4c832320808cd5caba7967907674576c174
chandannaresh <chandansutradhar2@gmail.com>
2023-05-05 15:25:04 +0530

    intro to JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        backgroundColor: "lightblue",
+      }}>
+      <h1>Welcome to Audit Management System</h1>
+      <button>Click Me</button>
     </div>
   );
 }
 
 export default App;
+
+function demo() {
+  let a = 10;
+  let b = 20;
+
+  return [a, b];
+}
+
+console.log(demo());
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'; //react library for managing UI state, props ,jsx
+import ReactDOM from 'react-dom/client'; //for virtual dom
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
